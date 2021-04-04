ece0b68b9cfebf7449e88008ba43992297482703
AnggunWardaniM <3103119024@student.smktelkom-pwt.sch.id>
2021-04-04 16:20:56 +0700

    first commit

---
+++ app/src/main/java/com/ndun/dasboard_mobileiu/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ndun.dasboard_mobileiu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
